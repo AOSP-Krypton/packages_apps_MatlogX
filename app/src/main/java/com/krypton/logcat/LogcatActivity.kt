f21bad5c157e7e6b25a85bbe9b479aff5a87a511
jhonboy121 <alfredmathew05@gmail.com>
2021-11-13 13:17:20 +0530

    Logcat: init project with basic activity
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

---
+++ app/src/main/java/com/krypton/logcat/LogcatActivity.kt
@@ -0,0 +1,12 @@
+package com.krypton.logcat
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LogcatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logcat)
+    }
+}
