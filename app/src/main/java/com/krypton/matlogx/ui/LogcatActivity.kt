f6843a99c52726af46d3348adba88f9c4a4255a0
jhonboy121 <alfredmathew05@gmail.com>
2022-01-18 01:18:53 +0530

    MatlogX: include icon for expand logs menu item
    * Same as in Matlog
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- app/src/main/java/com/krypton/matlogx/ui/LogcatActivity.kt
+++ app/src/main/java/com/krypton/matlogx/ui/LogcatActivity.kt
@@ -234,7 +234,13 @@ class LogcatActivity : AppCompatActivity() {
         )
         expandLogsMenuItem = menu.findItem(R.id.expand_logs)
         logcatViewModel.expandLogsLiveData.observe(this) {
-            expandLogsMenuItem.setTitle(if (it) R.string.minify_logs else R.string.expand_logs)
+            if (it) {
+                expandLogsMenuItem.setTitle(R.string.minify_logs)
+                expandLogsMenuItem.setIcon(R.drawable.ic_baseline_arrow_up_24)
+            } else {
+                expandLogsMenuItem.setTitle(R.string.expand_logs)
+                expandLogsMenuItem.setIcon(R.drawable.ic_baseline_arrow_down_24)
+            }
             logcatListAdapter.notifyItemRangeChanged(0, logcatListAdapter.itemCount)
         }
         val searchManager = getSystemService(SearchManager::class.java)
